d8a6efa6c8d920546ad744727515e4df52d8aa2c
salehahmed325 <salehahmed904@gmail.com>
2024-09-24 14:43:51 +0600

    Second commit with half done project

---
+++ js/triangle-calculate.js
@@ -0,0 +1,18 @@
+function calculateTriangleArea(){
+
+    //get value of triangle base
+    const triangleBaseInput = document.getElementById('triangle-base').value;
+    const base = parseFloat(triangleBaseInput);
+
+    //get value of triangle height
+    const triangleHeightInput = document.getElementById('triangle-height').value;
+    const height = parseFloat(triangleHeightInput);
+
+    //calculate the triangle area
+    const triangleArea = 0.5 * base * height;
+    console.log(triangleArea);
+
+    //display the triangle area on the output field
+    const displayTriangleArea = document.getElementById('triangle-are');
+    displayTriangleArea.innerText = triangleArea;
+}
